f5f1d3bb2563d76bcf1fd674c07882ffc8db420f
789ni812 <devbobbobbob@gmail.com>
2020-06-05 21:53:56 +0100

    Minor code cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+// Material-ui
+import Grid from '@material-ui/core/Grid';
+
 
 import './App.scss';
-import Grid from '@material-ui/core/Grid';
-import Header from './mockup/Header'
+
 import Footer from './components/Footer';
 //import ChartExample from './mockup/chartExample'; // show off some information in chart. Dont know what though?
 import CareerHistory from './mockup/careerHistory';
 //import UserDetailsTest from './mockup/test_usersDetails'; // navigator useragent information
-//import ContentIntroduction from './mockup/ContentIntroduction';
+import ContentIntroduction from './mockup/ContentIntroduction';
 
+
+function Home() {
+  return <ContentIntroduction />;
+}
+
+function Portfolio() {
+  return <h2>Portfolio</h2>;
+}
+
+function CHistory() {
+  return <CareerHistory />
+}
 
 const App = () => {
 
@@ -17,11 +40,46 @@ const App = () => {
     <div>
 
       <Grid container spacing={3}>
-        <Grid item xs={12}><Header /></Grid>
         <Grid item xs={12} >
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/portfolio">Portfolio</Link>
+                  </li>
+                  <li>
+                    <Link to="/careerHistory">Career history</Link>
+                  </li>
+                </ul>
+              </nav>
+              <Switch>
+                <Route path="/portfolio">
+                  <Portfolio />
+                </Route>
+                <Route path="/careerHistory">
+                  <CHistory />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
 
 
-          <CareerHistory />
+
+
+
+
+
+
+
+
+
 
 
 
