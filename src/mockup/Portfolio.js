86661e202604aa9d7da3a379d7d895a9daacd339
789ni812 <devbobbobbob@gmail.com>
2020-06-06 15:36:15 +0100

    Starting working on the Portfolio page, added ProdPom.com and Scr1pt0.com rough content

--- src/mockup/Portfolio.js
+++ src/mockup/Portfolio.js
@@ -12,7 +12,33 @@ const Portfolio = () => {
             < Card >
                 <CardContent>
                     <Typography variant="body2" color="textSecondary" component="p">
-                        <p>Porty Mc Portfolio</p>
+                        <p>Scr1pt0.com</p>
+                    </Typography>
+                </CardContent>
+            </Card >
+            < Card >
+                <CardContent>
+                    <Typography variant="h2" color="textSecondary" component="h2">
+                        <h2>ProdPom.com</h2>
+                    </Typography>
+                    <Typography variant="body1" color="textSecondary">
+                        <h2>ProdPom.com</h2>
+                        <p>Playing around with productivity and task managing. Heavily influenced from my Agile Project Management days coaching and supporting Agile development teams.</p>
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                        <p>Built with:</p>
+
+                        <ul>
+                            <li className="devicons devicons-nodejs">Node</li>
+                            <li className="devicons devicons-javascript">JavaScript</li>
+                            <li className="devicons devicons-mongo">Mongo</li>
+                            <li className="devicons devicons-html5">HTML 5</li>
+                            <li className="devicons devicons-css3">CSS3</li>
+                            <li className="devicons devicons-git">Git</li>
+
+                            //TODO >> Last edits made >> Portfolio 
+                            <li className="devicons devicons-reactjs">Raect</li>
+                        </ul>
                     </Typography>
                 </CardContent>
             </Card >
