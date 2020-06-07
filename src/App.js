dac663d0bfd5a7ed80c7d0365716db7e4cc95080
789ni812 <devbobbobbob@gmail.com>
2020-06-07 16:08:27 +0100

    Started adding the AppBar and Menu for the main navigation

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import {
 
 
 
-// Material-ui
-import Grid from '@material-ui/core/Grid';
-
 
 import './App.scss';
 
@@ -21,6 +18,76 @@ import CareerHistory from './mockup/careerHistory';
 //import UserDetailsTest from './mockup/test_usersDetails'; // navigator useragent information
 import ContentIntroduction from './mockup/ContentIntroduction';
 import PortfolioContent from './mockup/Portfolio';
+
+
+// Material-ui
+import Grid from '@material-ui/core/Grid';
+
+//START Material-ui AppBar
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+//END Material-ui AppBar
+
 
 
 function Home() {
@@ -36,11 +103,87 @@ function EmploymentHistory() {
 }
 
 const App = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
   return (
     <div>
       <Grid container spacing={3}>
         <Grid item xs={12} >
+
+
+
+
+          <div>
+            <Button
+              aria-controls="customized-menu"
+              aria-haspopup="true"
+              variant="contained"
+              color="primary"
+              onClick={handleClick}
+            >
+              Open Menu
+        </Button>
+            <StyledMenu
+              id="customized-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <StyledMenuItem>
+                <ListItemIcon>
+                  <SendIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Sent mail" />
+              </StyledMenuItem>
+              <StyledMenuItem>
+                <ListItemIcon>
+                  <DraftsIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Drafts" />
+              </StyledMenuItem>
+              <StyledMenuItem>
+                <ListItemIcon>
+                  <InboxIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+              </StyledMenuItem>
+            </StyledMenu>
+          </div>
+
+
+
+
+
+
+
+
+          <div className={classes.root}>
+            <AppBar position="fixed">
+              <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  News
+              </Typography>
+                <Button color="inherit">Login</Button>
+              </Toolbar>
+            </AppBar>
+          </div>
+
+
+
+
+
           <Router>
             <div>
               <nav>
