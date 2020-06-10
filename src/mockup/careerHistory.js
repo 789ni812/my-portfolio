942d11614e3d83c73561224c632c1cdbb019541c
789ni812 <devbobbobbob@gmail.com>
2020-06-10 21:09:47 +0100

    reduced redundant spacing

--- src/mockup/careerHistory.js
+++ src/mockup/careerHistory.js
@@ -95,9 +95,6 @@ const CareerHistory = () => {
                     </Card >
                     {/* END MAIN CONTENT LOOP */}
 
-
-
-
                 </ExpansionPanelDetails>
             </ExpansionPanel>
 
