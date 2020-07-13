0a76201af3797ea46e4fc90af9e290f31d584b3d
789ni812 <devbobbobbob@gmail.com>
2020-07-13 14:28:30 +0100

    Reduced the amount of Material-ui card components in the layout

--- src/mockup/ContentIntroduction.js
+++ src/mockup/ContentIntroduction.js
@@ -25,13 +25,7 @@ const ContentIntroduction = () => {
                     <h1>Hi,</h1>
                     <h2>I'm Stuart</h2>
                     <MdFormatQuote />Over the past 20 yesrs I've helped some great companies build and deliver digital projects<MdFormatQuote />
-                </CardContent>
 
-            </Card>
-
-
-            <Card >
-                <CardContent>
 
                     <h2>Working as:</h2>
 
@@ -84,58 +78,57 @@ const ContentIntroduction = () => {
 
                     </Grid>
 
-                </CardContent>
-
-
-            </Card>
-            <Card>
-
-                <p>Working with some great people across varied sectors</p>
 
 
 
 
-                <h3>including: </h3>
+                    <p>Working with some great people across varied sectors</p>
 
-                <Grid container spacing={2}>
-                    <Grid item xs={6} sm={2}>
-                        Startups
+
+
+
+                    <h3>including: </h3>
+
+                    <Grid container spacing={2}>
+                        <Grid item xs={6} sm={2}>
+                            Startups
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdLocationCity /> Government
+                        <Grid item xs={6} sm={2}>
+                            <MdLocationCity /> Government
                     </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdAccountBalance /> Banks
+                        <Grid item xs={6} sm={2}>
+                            <MdAccountBalance /> Banks
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdLocalHospital /> Health
+                        <Grid item xs={6} sm={2}>
+                            <MdLocalHospital /> Health
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdLocalLibrary /> Education
+                        <Grid item xs={6} sm={2}>
+                            <MdLocalLibrary /> Education
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        Sport
+                        <Grid item xs={6} sm={2}>
+                            Sport
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdShoppingCart /> Ecommerce
+                        <Grid item xs={6} sm={2}>
+                            <MdShoppingCart /> Ecommerce
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdAudiotrack /> Music
+                        <Grid item xs={6} sm={2}>
+                            <MdAudiotrack /> Music
                         </Grid>
 
-                    <Grid item xs={6} sm={2}>
-                        <MdBusiness /> Global Brands
+                        <Grid item xs={6} sm={2}>
+                            <MdBusiness /> Global Brands
                         </Grid>
 
-                </Grid>
-                <Divider />
+                    </Grid>
+                    <Divider />
+                </CardContent>
             </Card>
         </>
     )
