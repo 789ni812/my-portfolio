e63d73f87672134ee91ee6e48b85b503650fea61
789ni812 <devbobbobbob@gmail.com>
2020-09-16 18:10:19 +0100

    Updates

---
+++ precache-manifest.8abb1f109be53c6119184ebd85f6be9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f051eee7624fe12f2b35fecaab981025",
+    "url": "/stuartbradford/index.html"
+  },
+  {
+    "revision": "6da75e3abd610a6a03d4",
+    "url": "/stuartbradford/static/js/2.9ce81f5a.chunk.js"
+  },
+  {
+    "revision": "228bac5b7587832345b091357f4a52ce",
+    "url": "/stuartbradford/static/js/2.9ce81f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1ffb0cbfef0c3e38ec",
+    "url": "/stuartbradford/static/js/main.a4e3cc75.chunk.js"
+  },
+  {
+    "revision": "2b08abf7118c3407eef5",
+    "url": "/stuartbradford/static/js/runtime-main.b3a4428e.js"
+  }
+]);
