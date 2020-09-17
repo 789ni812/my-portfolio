cb95bcb0079756c7c3728df149855cda5f324808
789ni812 <devbobbobbob@gmail.com>
2020-09-17 08:43:15 +0100

    Updates

---
+++ precache-manifest.d2cae0012db670cec596ff94e78b3dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cce66bf4f8c6a8513662e87b8af4fa",
+    "url": "/stuartbradford/index.html"
+  },
+  {
+    "revision": "b15994974c4ec5bb9faa",
+    "url": "/stuartbradford/static/js/2.893809ae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/stuartbradford/static/js/2.893809ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d374b8a0d8c04e5747eb",
+    "url": "/stuartbradford/static/js/main.b9583ae2.chunk.js"
+  },
+  {
+    "revision": "2b08abf7118c3407eef5",
+    "url": "/stuartbradford/static/js/runtime-main.b3a4428e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stuartbradford/precache-manifest.8abb1f109be53c6119184ebd85f6be9b.js"
+  "/stuartbradford/precache-manifest.d2cae0012db670cec596ff94e78b3dd5.js"
 );
 
 self.addEventListener('message', (event) => {
