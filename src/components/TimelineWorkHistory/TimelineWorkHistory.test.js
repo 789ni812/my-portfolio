a562f8736bc2b6ca7e7d3a584b6cfd53f977f5b2
devBob <5435563+789ni812@users.noreply.github.com>
2021-01-04 16:52:04 +0000

    Added findByTestAttr and test to check render of demo timeline component

--- src/components/TimelineWorkHistory/TimelineWorkHistory.test.js
+++ src/components/TimelineWorkHistory/TimelineWorkHistory.test.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
+import { findByTestAttr } from "../../Utils/testUtils";
 import TimelineWorkHistory from "./TimelineWorkHistory.js";
 
+let wrapper;
+beforeAll(() => {
+  wrapper = shallow(<TimelineWorkHistory />);
+});
 it("should render without crashing", () => {
-  const component = shallow(<TimelineWorkHistory />);
+  const component = wrapper;
+  expect(component.length).toBe(1);
+});
+
+it("Renders demo of Timeline", () => {
+  const component = findByTestAttr(wrapper, "component-timelineDemo");
   expect(component.length).toBe(1);
 });
