29a0ab0584c67ae7f9d41fff3f8594a548f7a480
devBob <5435563+789ni812@users.noreply.github.com>
2021-01-11 13:35:29 +0000

    Added mediaQueris and adjusted the logo text based on screen size

--- src/components/ui/Header/header.js
+++ src/components/ui/Header/header.js
@@ -4,10 +4,18 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
+import { useMediaQuery } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  logoText: {
-    color: "red",
+  logo: {
+    padding: "0.2em",
+    fontSize: "1.75rem",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "1.50rem",
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.25rem",
+    },
   },
 }));
 
@@ -18,9 +26,7 @@ const Header = () => {
     <div>
       <AppBar position="fixed">
         <Toolbar disableGutters>
-          <Typography className={classes.logoText}>
-            StuartBradford.com
-          </Typography>
+          <Typography className={classes.logo}>StuartBradford.com</Typography>
         </Toolbar>
       </AppBar>
     </div>
