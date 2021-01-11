38b7df5950ece49c0504b46a5659bdeb5247b24c
devBob <5435563+789ni812@users.noreply.github.com>
2021-01-11 13:16:51 +0000

    Wired up styles. Made LogoText red

--- src/components/ui/Header/header.js
+++ src/components/ui/Header/header.js
@@ -1,14 +1,26 @@
 import React from "react";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  logoText: {
+    color: "red",
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h3">StuartBradford.com</Typography>
+        <Toolbar disableGutters>
+          <Typography className={classes.logoText}>
+            StuartBradford.com
+          </Typography>
         </Toolbar>
       </AppBar>
     </div>
