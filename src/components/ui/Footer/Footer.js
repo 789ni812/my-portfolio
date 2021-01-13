fb7b0250b5dda6724378b9b2c46295f5aed287b9
devBob <5435563+789ni812@users.noreply.github.com>
2021-01-13 13:57:17 +0000

    removed old Card imports

--- src/components/ui/Footer/Footer.js
+++ src/components/ui/Footer/Footer.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, Typography } from "@material-ui/core";
-import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 
 import { AiFillLinkedin } from "react-icons/ai";
