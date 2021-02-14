34a55af595d55506d15aebc1ff5988e9cab7cb32
devBob <5435563+789ni812@users.noreply.github.com>
2021-02-14 08:43:33 +0000

    Further Materialui styling to recentProject component

--- src/components/RecentProjects/RecentProjects.js
+++ src/components/RecentProjects/RecentProjects.js
@@ -18,16 +18,39 @@ import { AiFillApi } from "react-icons/ai";
 import DevBobApp from "../../assets/images/Devbob-app.png";
 import QuizApp from "../../assets/images/Quiz-app.png";
 
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import { green } from "@material-ui/core/colors";
+
 const useStyles = makeStyles((theme) => ({
   listGridContainer: {
     display: "flex",
     alignItems: "center",
     flexWrap: "flex",
   },
+  root: {
+    display: "flex",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  content: {
+    flex: "1 0 auto",
+  },
+
+  rounded: {
+    width: 180,
+    height: 180,
+    color: "#fff",
+    backgroundColor: green[500],
+  },
 }));
 
 const RecentProjects = () => {
   const classes = useStyles();
+
   return (
     <>
       <Typography variant="h3">Recent projects</Typography>
@@ -41,183 +64,258 @@ const RecentProjects = () => {
         <a href="https://github.com/789ni812">https://github.com/789ni812</a>
       </Typography>
 
-      <Typography variant="h3">Open Quiz </Typography>
-      <Typography variant="body1">
-        A quiz app allowing the user to answer multiple choice questions based
-        on a chosen category.
-      </Typography>
-      <img src={QuizApp} alt="Quiz App screenshot" width="175" height="175" />
-
-      <ul>
-        <li>Checks to see if an opentdb token exists in localstorage</li>
-        <li>
-          Fetching of OpenTDB token to ensure duplicate questions are not
-          retrieved
-        </li>
-        <li>fetches questions based on user selected category</li>
-        <li>
-          Merges and then randomises the correct and incorrect answers together
-        </li>
-        <li>Manages round score and session score</li>
-      </ul>
-
-      <Typography variant="h4">Technology Stack</Typography>
-
       <Grid
         container
-        spacing={2}
-        className={classes.listGridContainer}
-        justify="space-around"
+        direction="row"
+        justify="space-evenly"
+        alignItems="flex-start"
       >
         <Grid item>
-          <AiFillApi className={classes.icon} />
-          <br /> OpenTDB API
-        </Grid>
+          <Card>
+            <CardContent className={classes.content}>
+              <Typography component="h5" variant="h5">
+                Open Quiz{" "}
+              </Typography>
+              <Typography variant="subtitle1">
+                A quiz app allowing the user to answer multiple choice questions
+                based on a chosen category.
+              </Typography>
 
-        <Grid item>
-          <DiHtml5 className={classes.icon} />
-          <br /> HTML
-        </Grid>
-        <Grid item>
-          <DiCss3 className={classes.icon} />
-          <br /> CSS
-        </Grid>
-        <Grid item>
-          <DiJavascript className={classes.icon} />
-          <br /> JavaScript
-        </Grid>
-        <Grid item>
-          <DiReact className={classes.icon} />
-          <br /> React
-        </Grid>
+              <Grid
+                container
+                direction="row"
+                spacing={3}
+                justify="space-evenly"
+                alignItems="center"
+              >
+                <Grid item>
+                  <Avatar variant="rounded" className={classes.rounded}>
+                    <img
+                      src={QuizApp}
+                      alt="Quiz App screenshot"
+                      width="175"
+                      height="175"
+                    />
+                  </Avatar>
+                </Grid>
+                <Grid item>
+                  <ul>
+                    <li>
+                      Checks to see if an opentdb token exists in localstorage
+                    </li>
+                    <li>
+                      Fetching of OpenTDB token to ensure duplicate questions
+                      are not retrieved
+                    </li>
+                    <li>fetches questions based on user selected category</li>
+                    <li>
+                      Merges and then randomises the correct and incorrect
+                      answers together
+                    </li>
+                    <li>Manages round score and session score</li>
+                  </ul>
+                </Grid>
+              </Grid>
 
-        <Grid item>
-          <DiNodejs className={classes.icon} />
-          <br /> NodeJS
-        </Grid>
-        <Grid item>
-          <DiDrupal className={classes.icon} />
-          <br /> Drupal
-        </Grid>
-        <Grid item>
-          <DiDebian className={classes.icon} />
-          <br /> Debian
-        </Grid>
-      </Grid>
+              <Typography variant="subtitle1">Technology Stack</Typography>
 
-      <hr />
-      <Typography variant="h3">DevBob Database </Typography>
-      <Typography variant="body1">
-        Taking advantage of Drupal 9's CMS and headless capabilities. It makes
-        sense to me to use Drupal 9 CMS environment for editors and admin to
-        create and edit content. While JavaScript and React can interact with
-        Drupal's headless capabilities. The combination of Drupal and React
-        gives a rapid change environment and strong level of security.
-      </Typography>
-      <img src={DevBobApp} alt="Devbob screenshot" width="175" height="175" />
+              <Grid
+                container
+                spacing={2}
+                className={classes.listGridContainer}
+                justify="space-around"
+              >
+                <Grid item>
+                  <AiFillApi className={classes.icon} />
+                  <br /> OpenTDB API
+                </Grid>
 
-      <ul>
-        <li>Easy and secure editor environment in Drupal</li>
-        <li>
-          Fast prototyping is helped with rapid creation of Content Types,
-          Taxonomies, Topics and Authentication
-        </li>
-        <li>React fetches data from drupal to render on the front end</li>
-        <li>Multiple front end environments utilise data stored in Drupal</li>
-      </ul>
+                <Grid item>
+                  <DiHtml5 className={classes.icon} />
+                  <br /> HTML
+                </Grid>
+                <Grid item>
+                  <DiCss3 className={classes.icon} />
+                  <br /> CSS
+                </Grid>
+                <Grid item>
+                  <DiJavascript className={classes.icon} />
+                  <br /> JavaScript
+                </Grid>
+                <Grid item>
+                  <DiReact className={classes.icon} />
+                  <br /> React
+                </Grid>
 
-      <Typography variant="h4">Technology Stack</Typography>
+                <Grid item>
+                  <DiNodejs className={classes.icon} />
+                  <br /> NodeJS
+                </Grid>
+                <Grid item>
+                  <DiDrupal className={classes.icon} />
+                  <br /> Drupal
+                </Grid>
+                <Grid item>
+                  <DiDebian className={classes.icon} />
+                  <br /> Debian
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+          <Grid item>
+            <Card>
+              <CardContent className={classes.content}>
+                <Typography component="h5" variant="h5">
+                  DevBob Database{" "}
+                </Typography>
+                <Typography variant="subtitle1">
+                  A big fan of Drupal CMS since version 4.5 and I'm really
+                  enjoying Drupal 9 CMS and it's headless capabilities when
+                  using React on the front end. The combination of Drupal and
+                  React gives a rapid change environment, easy author editing
+                  and admin with a strong level of security.
+                </Typography>
 
-      <Grid
-        container
-        spacing={2}
-        className={classes.listGridContainer}
-        justify="space-around"
-      >
-        <Grid item>
-          <DiHtml5 className={classes.icon} />
-          <br /> HTML
-        </Grid>
-        <Grid item>
-          <DiCss3 className={classes.icon} />
-          <br /> CSS
-        </Grid>
-        <Grid item>
-          <DiJavascript className={classes.icon} />
-          <br /> JavaScript
-        </Grid>
-        <Grid item>
-          <DiReact className={classes.icon} />
-          <br /> React
-        </Grid>
-        <Grid item>
-          <DiNodejs className={classes.icon} />
-          <br /> NodeJS
-        </Grid>
-        <Grid item>
-          <DiDrupal className={classes.icon} />
-          <br /> Drupal
-        </Grid>
-        <Grid item>
-          <AiFillApi className={classes.icon} />
-          <br /> Headless Drupal API
-        </Grid>
+                <Grid
+                  container
+                  direction="row"
+                  spacing={3}
+                  justify="space-evenly"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <Avatar variant="rounded" className={classes.rounded}>
+                      <img
+                        src={DevBobApp}
+                        alt="Devbob screenshot"
+                        width="175"
+                        height="175"
+                      />
+                    </Avatar>
+                  </Grid>
+                  <Grid item>
+                    <ul>
+                      <li>Easy and secure editor environment in Drupal</li>
+                      <li>
+                        Rapid creation of Content Types, Taxonomies, Topics and
+                        Authentication
+                      </li>
+                      <li>
+                        React fetches data from drupal to render on the front
+                        end
+                      </li>
+                      <li>
+                        Multiple front end environments utilise data stored in
+                        Drupal
+                      </li>
+                    </ul>
+                  </Grid>
+                </Grid>
 
-        <Grid item>
-          <DiDebian className={classes.icon} />
-          <br /> Debian
-        </Grid>
-      </Grid>
+                <Typography variant="h4">Technology Stack</Typography>
 
-      <hr />
-      <Typography variant="h3">Rainy's Cycle App </Typography>
-      <Typography variant="body1">
-        Location based realtime map and weather app to help cyclists naviagte
-        directions and weather whilst out on a bike tour. Unlike many other
-        apps, the focus is less on fitness and more on the enjoyment of getting
-        lost on a bike and finding the best spot for a sandwich and a coffee.
-      </Typography>
+                <Grid
+                  container
+                  spacing={2}
+                  className={classes.listGridContainer}
+                  justify="space-around"
+                >
+                  <Grid item>
+                    <DiHtml5 className={classes.icon} />
+                    <br /> HTML
+                  </Grid>
+                  <Grid item>
+                    <DiCss3 className={classes.icon} />
+                    <br /> CSS
+                  </Grid>
+                  <Grid item>
+                    <DiJavascript className={classes.icon} />
+                    <br /> JavaScript
+                  </Grid>
+                  <Grid item>
+                    <DiReact className={classes.icon} />
+                    <br /> React
+                  </Grid>
+                  <Grid item>
+                    <DiNodejs className={classes.icon} />
+                    <br /> NodeJS
+                  </Grid>
+                  <Grid item>
+                    <DiDrupal className={classes.icon} />
+                    <br /> Drupal
+                  </Grid>
+                  <Grid item>
+                    <AiFillApi className={classes.icon} />
+                    <br /> Headless Drupal API
+                  </Grid>
 
-      <Typography variant="h4">Technology Stack</Typography>
+                  <Grid item>
+                    <DiDebian className={classes.icon} />
+                    <br /> Debian
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Card>
+              <CardContent className={classes.content}>
+                <Typography component="h5" variant="h5">
+                  Rainy's Cycle App{" "}
+                </Typography>
+                <Typography variant="subtitle1">
+                  Location based realtime map and weather app to help cyclists
+                  naviagte directions and weather whilst out on a bike tour.
+                  Unlike many other apps, the focus is less on fitness and more
+                  on the enjoyment of getting lost on a bike and finding the
+                  best spot for a sandwich and a coffee.
+                </Typography>
 
-      <Grid
-        container
-        spacing={2}
-        className={classes.listGridContainer}
-        justify="space-around"
-      >
-        <Grid item>
-          <DiHtml5 className={classes.icon} />
-          <br /> HTML
-        </Grid>
-        <Grid item>
-          <DiCss3 className={classes.icon} />
-          <br /> CSS
-        </Grid>
-        <Grid item>
-          <DiJavascript className={classes.icon} />
-          <br /> JavaScript
-        </Grid>
-        <Grid item>
-          <DiReact className={classes.icon} />
-          <br /> React
-        </Grid>
-        <Grid item>
-          <DiNodejs className={classes.icon} />
-          <br /> NodeJS
-        </Grid>
-        <Grid item>
-          <AiFillApi className={classes.icon} />
-          <br /> Leaflet API
-        </Grid>
+                <Typography variant="h4">Technology Stack</Typography>
 
-        <Grid item>
-          <DiMongodb className={classes.icon} />
-          <br /> Mongo
-        </Grid>
-        <Grid item>
-          <DiDebian className={classes.icon} />
-          <br /> Debian
+                <Grid
+                  container
+                  spacing={2}
+                  className={classes.listGridContainer}
+                  justify="space-around"
+                >
+                  <Grid item>
+                    <DiHtml5 className={classes.icon} />
+                    <br /> HTML
+                  </Grid>
+                  <Grid item>
+                    <DiCss3 className={classes.icon} />
+                    <br /> CSS
+                  </Grid>
+                  <Grid item>
+                    <DiJavascript className={classes.icon} />
+                    <br /> JavaScript
+                  </Grid>
+                  <Grid item>
+                    <DiReact className={classes.icon} />
+                    <br /> React
+                  </Grid>
+                  <Grid item>
+                    <DiNodejs className={classes.icon} />
+                    <br /> NodeJS
+                  </Grid>
+                  <Grid item>
+                    <AiFillApi className={classes.icon} />
+                    <br /> Leaflet API
+                  </Grid>
+
+                  <Grid item>
+                    <DiMongodb className={classes.icon} />
+                    <br /> Mongo
+                  </Grid>
+                  <Grid item>
+                    <DiDebian className={classes.icon} />
+                    <br /> Debian
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </>
